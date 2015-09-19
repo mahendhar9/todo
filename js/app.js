6063ca585d98ccb50e1e1f7a3e039e67c9e532f6
Mahendhar <mahendarreddy@live.com>
2015-09-19 14:04:09 +0530

    Ability to add and delete todos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 var todoApp = angular.module('todoApp',[]);
 
 todoApp.controller('todoController', ['$scope', function($scope){
+  $scope.todos = [
+  {done: false, text: "First Todo"},
+  {done: false, text: "Second Todo"}
+  ];
+
+  $scope.addTodo = function() {
+    var newTodo = {
+      done: false,
+      text: $scope.newTodoText
+    };
+
+    $scope.todos.push(newTodo);
+    $scope.newTodoText = '';
+  };
+
+  $scope.deleteTask = function(start) {
+    $scope.todos.splice(start, 1);
+  };
 
 }]);
