5e83db31b35fd08b4ac137a7b7696b7dd12c13b7
Mahendhar <mahendarreddy@live.com>
2015-09-19 14:42:53 +0530

    Todos can now be moved down

--- js/app.js
+++ js/app.js
@@ -11,13 +11,18 @@ todoApp.controller('todoController', ['$scope', function($scope){
       done: false,
       text: $scope.newTodoText
     };
-
     $scope.todos.push(newTodo);
     $scope.newTodoText = '';
   };
 
   $scope.deleteTask = function(start) {
     $scope.todos.splice(start, 1);
+  };
+
+  $scope.moveDown = function(index) {
+    var downItem = $scope.todos[index];
+    $scope.todos.splice(index + 2, 0, downItem);
+    $scope.todos.splice(index, 1);
   };
 
 }]);
